9ec86ebdd70a74bff49dc2494d801590aa9c5bf8
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-05-31 00:20:31 +0300

    Update geoseq.c

--- geoseq.c
+++ geoseq.c
@@ -3,7 +3,7 @@
 #include <stdint.h>
 typedef long double f96;
 int main(int argc,char**argv){
-if(argc<4){puts("SYNTAX:geoseq init_num mult steps [\"separator\"]\n Creates geometric sequences starting from num(treated as double rounded to integer) with N steps, each multiplying num by 'mult'\n separator is newline by default \nExample:./geoseq  20 1.01 4 \nResult:20 20 20 21");return 1;}
+if(argc<4){puts("SYNTAX:geoseq init_num mult steps [\"separator\"]\n Creates geometric sequences starting from num\n(treated as double rounded to integer) with N steps,\n each multiplying num by 'mult'\n [optional] separator is newline by default \nExample:./geoseq  20 1.01 4 \nResult:20 20 20 21");return 1;}
 f96 i=strtold(argv[1],NULL);
 f96 mult=strtold(argv[2],NULL);
 char* sep=argc==5?argv[4]:"\n";
@@ -12,4 +12,3 @@ printf("%.0Lf",i);
 for(uint64_t l=1;l<count;l++){
 i*=mult;printf("%s",sep);printf("%.0Lf",i);}
 return 0;}
-
