7144cd3094aa58041b5d4200e550ce2c5997b0dc
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-05-31 00:17:46 +0300

    add sep

--- geoseq.c
+++ geoseq.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include <inttypes.h>
 typedef long double f96;
 int main(int argc,char**argv){
-if(argc!=4){puts("SYNTAX:geoseq init_num mult steps\n Creates geometric sequences starting from num(treated as double rounded to integer) with N steps, each multiplying num by 'mult'  \nExample:./geoseq  20 1.01 4 \nResult:20 20 20 21");return 1;}
+if(argc<4){puts("SYNTAX:geoseq init_num mult steps [\"separator\"]\n Creates geometric sequences starting from num(treated as double rounded to integer) with N steps, each multiplying num by 'mult'\n separator is newline by default \nExample:./geoseq  20 1.01 4 \nResult:20 20 20 21");return 1;}
 f96 i=strtold(argv[1],NULL);
 f96 mult=strtold(argv[2],NULL);
+char* sep=argc==5?argv[4]:"\n";
 uint64_t count=strtoull(argv[3],NULL,10);
-printf("%.0Lf\n",i);
+printf("%.0Lf",i);
 for(uint64_t l=1;l<count;l++){
-i*=mult;printf("%.0Lf\n",i);}
+i*=mult;printf("%s",sep);printf("%.0Lf",i);}
 return 0;}
 
